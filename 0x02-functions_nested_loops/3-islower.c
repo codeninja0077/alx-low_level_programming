74ea269fd095172b37271cfa785d1b4222073ca1
“codeninja0077” <niiayiteybeing@gmail.com>
2022-04-14 19:52:27 +0000

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
