479804e23a8e8244ffe4f81278665ecf88f0c2eb
“codeninja0077” <niiayiteybeing@gmail.com>
2022-04-14 22:03:22 +0000

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int print_sign(int n)
+{
+int rtValue;
+
+if (n > 0)
+{
+	rtValue = 1;
+	_putchar('+');
+}
+else if (n == 0)
+{
+	rtValue = 0;
+	_putchar('0');
+}
+else
+{
+	rtValue = -1;
+	_putchar('-');
+}
+
+return (test);
+}
