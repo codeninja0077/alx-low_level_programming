76a57ed521cf019f6ecdc25f988409b9c4856b93
“codeninja0077” <niiayiteybeing@gmail.com>
2022-04-26 00:53:31 +0000

    task-6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts - prints values on a newline
+ * _puts2 - prints values on a newline
  * @str: char pointer
  * Return: void
  */
@@ -10,6 +10,10 @@ void _puts2(char *str)
 {
 	while (*str != '\0')
 	{
-		printf("%c \n", *str++);
+		if (*str % 2 == 0)
+		{
+			printf("%c", *str++);
+		}
+		printf("\n");
 	}
 }
