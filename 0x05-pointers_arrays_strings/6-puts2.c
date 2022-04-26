47c104f3f0cb883a5fbf34c7dc4639844b38b1c5
“codeninja0077” <niiayiteybeing@gmail.com>
2022-04-26 00:36:46 +0000

    task-6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints values on a newline
+ * @str: char pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		printf("%c \n", *str++);
+	}
+}
