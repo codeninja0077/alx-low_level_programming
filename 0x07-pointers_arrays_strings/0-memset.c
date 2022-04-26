fc4abd0ae0992fbf99a306fc3309a553aa984d62
“codeninja0077” <niiayiteybeing@gmail.com>
2022-04-26 23:25:34 +0000

    task-0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * memset - function to set a constant byte to memory
+ * _memset - function to set a constant byte to memory
  * @s: Pointer where the constant is assigned or set
  * @b: Holds constant value
  * @n: Holds the number of loops done
+ * Return:*s Pointer
  */
 
- char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for(i = 0: i < n : i++ )
+	for (int i = 0; i < n ; i++)
 	{
 		s[i] = b;
 	}
-	return(s);
+	return (s);
 }
